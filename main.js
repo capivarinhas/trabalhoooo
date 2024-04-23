7179e7f9f01aadf1c8252280a605c67e3461c15b
capivarinhas <134078456+capivarinhas@users.noreply.github.com>
2024-04-23 12:09:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelectorAll(botao);
+const textos = document.querySelectorAll(aba-conteudo);
+
+for (let i=0 < botoes.length;j++){
+    botoes[j].classlist.remove("ativo");
+   textos[j].classlist.remove("ativo");
+    
+
+}
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo")
